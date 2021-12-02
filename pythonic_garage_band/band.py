c78d224fe6ac2da4124acde5057c98972ab22f62
Joe Streifel <josephstreifel@gmail.com>
2021-12-02 14:48:11 -0800

    guitarist name and intro

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -7,7 +7,11 @@ class Musician:
 
 
 class Guitarist:
-    pass
+    def __init__(self, name):
+        self.name = name
+        
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
 
 
 class Bassist:
